84b96a61dec46613bd56794f1211e84d95c45272
Esmeralda Jezik <esmeraldajezik@gmail.com>
2026-06-03 18:57:05 -0300

    inicializacion de proyecto

---
+++ dominio/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+
+        public bool Activo { get; set; }
+
+        public Usuario Usuario { get; set; }
+    }
+}
