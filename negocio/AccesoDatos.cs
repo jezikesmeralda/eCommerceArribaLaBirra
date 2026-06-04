e2f36a862ce72556110f84f6df339ec9ca36734e
feliperosasco <98924125+feliperosasco@users.noreply.github.com>
2026-06-04 19:55:29 -0300

    Se agrega proyecto Negocio y Acceso a Datos

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using dominio;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            try
+            {
+                conexion = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=Comercio;Trusted_Connection=True;TrustServerCertificate=True;");
+                conexion.Open();
+            }
+            catch
+            {
+                conexion = new SqlConnection(@"Server=.\SQLEXPRESS;Database=Comercio;Integrated Security=False;User=sa;Password=Passw0rd2025!;TrustServerCertificate=True;");
+                conexion.Open();
+            }
+
+            conexion.Close();
+            comando = new SqlCommand();
+        }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.Parameters.Clear();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void SetearProcedimiento(string nombreSP)
+        {
+            comando.Parameters.Clear();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = nombreSP;
+        }
+
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        public void SetearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public object EjecutarScalar()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                return comando.ExecuteScalar();
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
